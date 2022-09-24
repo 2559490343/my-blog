d1953fa09f4abd0c93a033d00e0ed446177ff1db
xiongyingcheng <2559490343@qq.com>
2022-09-24 09:57:49 +0800

    AutoSelect组件迁入、样式修改

--- blog-client/src/components/write/components/editorForm/index.tsx
+++ blog-client/src/components/write/components/editorForm/index.tsx
@@ -1,7 +1,30 @@
+import AutoSelect from '@/components/AutoSelect';
 import { Form, Input, Cascader } from 'antd';
 
 export default function EditorForm(props: any) {
   const { editorForm } = props;
+  const tags = [
+    {
+      label: '前端',
+      value: '前端',
+    },
+    {
+      label: 'React',
+      value: 'React',
+    },
+    {
+      label: '后端',
+      value: '后端',
+    },
+    {
+      label: 'Java',
+      value: 'Java',
+    },
+    {
+      label: 'Vue',
+      value: 'Vue',
+    },
+  ];
   // 提交钩子
   const handleSubmit = (values: any) => {
     console.log(values);
@@ -28,6 +51,18 @@ export default function EditorForm(props: any) {
         ]}
       >
         <Input maxLength={30} placeholder="请输入" />
+      </Form.Item>
+      <Form.Item
+        label="标签"
+        name="tags"
+        rules={[
+          {
+            required: true,
+            message: '请选择标签!',
+          },
+        ]}
+      >
+        <AutoSelect options={tags} mode='multiple' />
       </Form.Item>
     </Form>
   );
